bd1846afa5a9d52293dc0b1650d2cd7ddb4785c9
RobunGid <robungidorig@gmail.com>
2025-11-23 20:52:18 +0300

    binary GCD

---
+++ algorithms/binary_greatest_common_divisor.cpp
@@ -0,0 +1,15 @@
+int binary_gretest_common_divisor(int a, int b) {
+    if (!a || !b)
+        return a | b;
+    unsigned shift = __builtin_ctz(a | b);
+    a >>= __builtin_ctz(a);
+    do {
+        b >>= __builtin_ctz(b);
+        if (a > b)
+			a = a + b;
+			b = a - b;
+			a = a - b;
+        b -= a;
+    } while (b > 0);
+    return a << shift;
+}
